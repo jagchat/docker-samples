57bb312fcac6439b0ecd8e5095195f29dd5d58a1
jagchat <jag.chat@gmail.com>
2019-05-24 11:55:26 -0500

    modified rabbitmq to include fanout method

--- images/rabbitmq/nodejs-sample/sample-publisher/send.js
+++ images/rabbitmq/nodejs-sample/sample-publisher/send.js
@@ -2,14 +2,23 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://172.21.136.192:5672', function(err, conn) {
+amqp.connect('amqp://pd-docker-dev.psfy.personifycorp.com:5672', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
-    var msg = 'Hello World!';
-
-    ch.assertQueue(q, {durable: false});
-    ch.sendToQueue(q, Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
+    ////uses round robin method for the consumers
+	////-a consumer gets one turn for one publish
+	////-second publish goes to second consumer (waiting meanwhile), similarly third
+	////-once a round is finished covering all consumers (for each/one publish at a time), the next publish goes to first consumer
+	//var q = 'hello';
+    //var msg = 'Hello World!';
+    //ch.assertQueue(q, {durable: false});
+    //ch.sendToQueue(q, Buffer.from(msg));
+    //console.log(" [x] Sent %s", msg);
+	
+	//uses fanout method (all subscribers would receive same message at once)
+	var exchange = 'global'; //any name
+	var msg = 'Hello World!';
+	ch.assertExchange(exchange, 'fanout', {durable: false});
+	ch.publish(exchange, '', Buffer.from(msg));
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
--- images/rabbitmq/nodejs-sample/sample-subscriber/receive.js
+++ images/rabbitmq/nodejs-sample/sample-subscriber/receive.js
@@ -2,14 +2,37 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://172.21.136.192:5672', function(err, conn) {
+amqp.connect('amqp://pd-docker-dev.psfy.personifycorp.com:5672', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    ////uses round robin method for the consumers
+	////-a consumer gets one turn for one publish
+	////-second publish goes to second consumer (waiting meanwhile), similarly third
+	////-once a round is finished covering all consumers (for each/one publish at a time), the next publish goes to first consumer
+	//var q = 'hello';
+    //ch.assertQueue(q, {durable: false});
+    //console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    //ch.consume(q, function(msg) {
+    //  console.log(" [x] Received %s", msg.content.toString());
+    //}, {noAck: true});
+	
+	
+	//uses fanout method (all subscribers would receive same message at once)
+	var exchange = 'global';
+    ch.assertExchange(exchange, 'fanout', {durable: false});
+    ch.assertQueue('', {exclusive: true }, function(error2, q) {
+		if (error2) {
+			throw error2;
+		}
+		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+		ch.bindQueue(q.queue, exchange, '');
 
-    ch.assertQueue(q, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    ch.consume(q, function(msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+		ch.consume(q.queue, function(msg) {
+			if(msg.content) {
+				console.log(" [x] Received %s", msg.content.toString());
+			}
+		}, 
+		{noAck: true});	
+	});
+	
   });
 });
